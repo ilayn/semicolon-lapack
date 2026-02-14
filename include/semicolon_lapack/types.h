f822a06e5cc0bab4fab02364a9d921d4e0f11a03
ilayn <ilhanpolat@gmail.com>
2026-02-14 17:02:39 +0100

    ENH:Add types header before type rename

---
+++ include/semicolon_lapack/types.h
@@ -0,0 +1,9 @@
+#ifndef SEMICOLON_TYPES_H
+#define SEMICOLON_TYPES_H
+
+typedef float          f32;
+typedef double         f64;
+typedef float _Complex  c64;
+typedef double _Complex c128;
+
+#endif /* SEMICOLON_TYPES_H */
