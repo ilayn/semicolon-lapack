36e3b9b0cf7e980c7707a9a78c5e35f9dca0e6b0
ilayn <ilhanpolat@gmail.com>
2026-07-18 19:11:37 +0200

    DOC: Overhaul sy/heev_2stage docs

--- src/c/cheev_2stage.c
+++ src/c/cheev_2stage.c
@@ -8,6 +8,88 @@
 #include "semicolon_cblas.h"
 #include <math.h>
 
+/**
+ * CHEEV_2STAGE computes all eigenvalues and, optionally, eigenvectors of a
+ * complex Hermitian matrix `A` using the 2stage technique for the reduction
+ * to tridiagonal.
+ *
+ * @param[in]     jobz  `'N'`: Compute eigenvalues only;
+ *                      `'V'`: Compute eigenvalues and eigenvectors.
+ *                             Not available in this release.
+ * @param[in]     uplo  `'U'`: Upper triangle of `A` is stored;
+ *                      `'L'`: Lower triangle of `A` is stored.
+ * @param[in]     n     The order of the matrix `A`. `n>=0`.
+ * @param[in,out] A     On entry, the Hermitian matrix `A`. If `uplo='U'`, the
+ *                      leading `n`-by-`n` upper triangular part of `A` contains the
+ *                      upper triangular part of the matrix `A`. If `uplo='L'`,
+ *                      the leading `n`-by-`n` lower triangular part of `A` contains
+ *                      the lower triangular part of the matrix `A`.
+ *                      On exit, if `jobz='V'`, then if `info=0`, `A` contains the
+ *                      orthonormal eigenvectors of the matrix `A`.
+ *                      If `jobz='N'`, then on exit the lower triangle (if `uplo='L'`)
+ *                      or the upper triangle (if `uplo='U'`) of `A`, including the
+ *                      diagonal, is destroyed.
+ * @param[in]     lda   The leading dimension of the array `A`. `lda>=max(1,n)`.
+ * @param[out]    W     Array of dimension (`n`). If `info=0`, the eigenvalues in
+ *                      ascending order.
+ * @param[out]    work  On exit, if `info=0`, `work[0]` returns the optimal `lwork`.
+ * @param[in]     lwork The length of the array `work`. `lwork>=1`, when `n<=1`;
+ *                      otherwise
+ *                      If `jobz='N'` and `n>1`, `lwork` must be queried.
+ *                      @rst
+ *                      .. code-block:: text
+ *
+ *                          lwork = MAX(1, dimension) where
+ *                          dimension = max(stage1,stage2) + (kd+1)*n + n
+ *                                    = n*kd + n*max(kd+1,FACTOPTNB)
+ *                                      + max(2*kd*kd, kd*NTHREADS)
+ *                                      + (kd+1)*n + n
+ *                      @endrst
+ *                      where `kd` is the blocking size of the reduction,
+ *                      FACTOPTNB is the blocking used by the QR or LQ
+ *                      algorithm, usually FACTOPTNB=128 is a good choice,
+ *                      NTHREADS is the number of threads used when openMP
+ *                      compilation is enabled, otherwise =1.
+ *                      If `jobz='V'` and `n>1`, `lwork` must be queried. Not yet available
+ *                      If `lwork=-1`, then a workspace query is assumed; the routine
+ *                      only calculates the optimal size of the `work` array, returns
+ *                      this value as the first entry of the `work` array, and no error
+ *                      message related to `lwork` is issued.
+ * @param[out]    rwork Array of dimension (max(1, 3*`n`-2)).
+ * @param[out]    info  `info=0`: successful exit
+ *                      `info<0`: if `info=-i`, the i-th argument had an illegal value
+ *                      `info>0`: if `info=i`, the algorithm failed to converge; i
+ *                                off-diagonal elements of an intermediate tridiagonal
+ *                                form did not converge to zero.
+ *
+ * @par Further Details:
+ * @rst
+ * All details about the 2stage techniques are available in:
+ *
+ * Azzam Haidar, Hatem Ltaief, and Jack Dongarra.
+ * Parallel reduction to condensed forms for symmetric eigenvalue problems
+ * using aggregated fine-grained and memory-aware kernels. In Proceedings
+ * of 2011 International Conference for High Performance Computing,
+ * Networking, Storage and Analysis (SC '11), New York, NY, USA,
+ * Article 8 , 11 pages.
+ * http://doi.acm.org/10.1145/2063384.2063394
+ *
+ * A. Haidar, J. Kurzak, P. Luszczek, 2013.
+ * An improved parallel singular value algorithm and its implementation
+ * for multicore hardware, In Proceedings of 2013 International Conference
+ * for High Performance Computing, Networking, Storage and Analysis (SC '13).
+ * Denver, Colorado, USA, 2013.
+ * Article 90, 12 pages.
+ * http://doi.acm.org/10.1145/2503210.2503292
+ *
+ * A. Haidar, R. Solca, S. Tomov, T. Schulthess and J. Dongarra.
+ * A novel hybrid CPU-GPU generalized eigensolver for electronic structure
+ * calculations based on fine-grained memory aware tasks.
+ * International Journal of High Performance Computing Applications.
+ * Volume 28 Issue 2, Pages 196-209, May 2014.
+ * https://doi.org/10.1177/1094342013502097
+ * @endrst
+ */
 void cheev_2stage(const char* jobz, const char* uplo, const INT n,
                   c64* A, const INT lda,
                   f32* W,
