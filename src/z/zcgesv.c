279e181cc03686aaafeb8a6c68e3d5df9d9cb53c
ilayn <ilhanpolat@gmail.com>
2026-07-19 12:24:31 +0200

    DOC: Overhaul dsgesv/zcgesv docs

--- src/z/zcgesv.c
+++ src/z/zcgesv.c
@@ -10,8 +10,12 @@
 
 /**
  * ZCGESV computes the solution to a complex system of linear equations
- *    A * X = B,
- * where A is an N-by-N matrix and X and B are N-by-NRHS matrices.
+ * @rst
+ * .. code-block:: text
+ *
+ *     A * X = B
+ * @endrst
+ * where `A` is an `n`-by-`n` matrix and `X` and `B` are `n`-by-`nrhs` matrices.
  *
  * ZCGESV first attempts to factorize the matrix in COMPLEX and use this
  * factorization within an iterative refinement procedure to produce a
@@ -19,42 +23,52 @@
  * If the approach fails the method switches to a COMPLEX*16
  * factorization and solve.
  *
- * The iterative refinement process is stopped if
+ * @rst
+ * The iterative refinement process is stopped if::
+ *
  *     ITER > ITERMAX
- * or for all the RHS we have:
+ *
+ * or for all the RHS we have::
+ *
  *     RNRM < SQRT(N)*XNRM*ANRM*EPS*BWDMAX
+ *
  * where
- *     o ITER is the number of the current iteration in the iterative
- *       refinement process
- *     o RNRM is the infinity-norm of the residual
- *     o XNRM is the infinity-norm of the solution
- *     o ANRM is the infinity-operator-norm of the matrix A
- *     o EPS is the machine epsilon returned by DLAMCH('Epsilon')
+ *
+ * - ITER is the number of the current iteration in the iterative
+ *   refinement process
+ * - RNRM is the infinity-norm of the residual
+ * - XNRM is the infinity-norm of the solution
+ * - ANRM is the infinity-operator-norm of the matrix ``A``
+ * - EPS is the machine epsilon returned by DLAMCH('Epsilon')
+ *
  * The value ITERMAX and BWDMAX are fixed to 30 and 1.0D+00
  * respectively.
+ * @endrst
  *
- * @param[in]     n     The number of linear equations, i.e., the order of A. n >= 0.
- * @param[in]     nrhs  The number of right hand sides. nrhs >= 0.
- * @param[in,out] A     On entry, the N-by-N coefficient matrix A.
- *                      On exit, if iterative refinement succeeded (iter >= 0), A is unchanged.
- *                      If COMPLEX*16 factorization was used (iter < 0), A contains
- *                      the factors L and U from A = P*L*U.
- *                      Array of dimension (lda, n).
- * @param[in]     lda   The leading dimension of A. lda >= max(1, n).
- * @param[out]    ipiv  The pivot indices. Array of dimension n, 0-based.
- * @param[in]     B     The N-by-NRHS right hand side matrix B.
- *                      Array of dimension (ldb, nrhs).
- * @param[in]     ldb   The leading dimension of B. ldb >= max(1, n).
- * @param[out]    X     If info = 0, the N-by-NRHS solution matrix X.
- *                      Array of dimension (ldx, nrhs).
- * @param[in]     ldx   The leading dimension of X. ldx >= max(1, n).
+ * @param[in]     n     The number of linear equations, i.e., the order of `A`.
+ *                      `n>=0`.
+ * @param[in]     nrhs  The number of right hand sides. `nrhs>=0`.
+ * @param[in,out] A     Array of dimension (`lda`, `n`).
+ *                      On entry, the `n`-by-`n` coefficient matrix `A`.
+ *                      On exit, if iterative refinement succeeded (`iter>=0`), `A`
+ *                      is unchanged. If COMPLEX*16 factorization was used
+ *                      (`iter<0`), `A` contains the factors L and U from A = P*L*U.
+ * @param[in]     lda   The leading dimension of `A`. `lda>=max(1,n)`.
+ * @param[out]    ipiv  Array of dimension `n`.
+ *                      The pivot indices.
+ * @param[in]     B     Array of dimension (`ldb`, `nrhs`).
+ *                      The `n`-by-`nrhs` right hand side matrix `B`.
+ * @param[in]     ldb   The leading dimension of `B`. `ldb>=max(1,n)`.
+ * @param[out]    X     Array of dimension (`ldx`, `nrhs`).
+ *                      If `info=0`, the `n`-by-`nrhs` solution matrix X.
+ * @param[in]     ldx   The leading dimension of `X`. `ldx>=max(1,n)`.
  * @param[out]    work  Complex*16 workspace for residual vectors.
- *                      Array of dimension (n, nrhs).
+ *                      Array of dimension (`n`, `nrhs`).
  * @param[out]    swork Complex (single precision) workspace for matrix and solutions.
- *                      Array of dimension n*(n+nrhs).
- * @param[out]    rwork Double precision array, dimension (n).
+ *                      Array of dimension `n*(n+nrhs)`.
+ * @param[out]    rwork Double precision array, dimension (`n`).
  * @param[out]    iter  Iteration count:
- *                      - < 0: iterative refinement has failed, COMPLEX*16
+ *                      - `iter<0`: iterative refinement has failed, COMPLEX*16
  *                        factorization has been performed
  *                        - -1 : the routine fell back to full precision for
  *                          implementation- or machine-specific reasons
@@ -63,13 +77,12 @@
  *                        - -3 : failure of CGETRF
  *                        - -31: stop the iterative refinement after the 30th
  *                          iterations
- *                      - > 0: iterative refinement has been successfully used.
+ *                      - `iter>0`: iterative refinement has been successfully used.
  *                        Returns the number of iterations
  * @param[out]    info
- *                           Exit status:
- *                           - = 0: successful exit
- *                           - < 0: if info = -i, argument i had an illegal value
- *                           - > 0: if info = i, U(i-1,i-1) is exactly zero
+ *                          - `info=0`: successful exit
+ *                          - `info<0`: if `info=-i`, argument i had an illegal value
+ *                          - `info>0`: if `info=i`, U(i,i) is exactly zero
  */
 void zcgesv(
     const INT n,
