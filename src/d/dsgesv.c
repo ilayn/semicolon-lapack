757ae3ad6c97b6a4debb47e8cb1885c407021b72
ilayn <ilhanpolat@gmail.com>
2026-07-19 12:24:31 +0200

    DOC: Overhaul dsgesv/zcgesv docs

--- src/d/dsgesv.c
+++ src/d/dsgesv.c
@@ -12,8 +12,12 @@
 
 /**
  * DSGESV computes the solution to a real system of linear equations
- *    A * X = B,
- * where A is an N-by-N matrix and X and B are N-by-NRHS matrices.
+ * @rst
+ * .. code-block:: text
+ *
+ *     A * X = B
+ * @endrst
+ * where `A` is an `n`-by-`n` matrix and `X` and `B` are `n`-by-`nrhs` matrices.
  *
  * DSGESV first attempts to factorize the matrix in SINGLE PRECISION
  * and use this factorization within an iterative refinement procedure
@@ -21,27 +25,29 @@
  * quality. If the approach fails the method switches to a DOUBLE PRECISION
  * factorization and solve.
  *
- * @param[in]     n     The number of linear equations, i.e., the order of A. n >= 0.
- * @param[in]     nrhs  The number of right hand sides. nrhs >= 0.
- * @param[in,out] A     On entry, the N-by-N coefficient matrix A.
- *                      On exit, if iterative refinement succeeded (iter >= 0), A is unchanged.
- *                      If f64 precision factorization was used (iter < 0), A contains
- *                      the factors L and U from A = P*L*U.
- *                      Array of dimension (lda, n).
- * @param[in]     lda   The leading dimension of A. lda >= max(1, n).
- * @param[out]    ipiv  The pivot indices. Array of dimension n, 0-based.
- * @param[in]     B     The N-by-NRHS right hand side matrix B.
- *                      Array of dimension (ldb, nrhs).
- * @param[in]     ldb   The leading dimension of B. ldb >= max(1, n).
- * @param[out]    X     If info = 0, the N-by-NRHS solution matrix X.
- *                      Array of dimension (ldx, nrhs).
- * @param[in]     ldx   The leading dimension of X. ldx >= max(1, n).
+ * @param[in]     n     The number of linear equations, i.e., the order of `A`.
+ *                      `n>=0`.
+ * @param[in]     nrhs  The number of right hand sides. `nrhs>=0`.
+ * @param[in,out] A     Array of dimension (`lda`, `n`).
+ *                      On entry, the `n`-by-`n` coefficient matrix `A`.
+ *                      On exit, if iterative refinement succeeded (`iter>=0`), `A`
+ *                      is unchanged. If double precision factorization was used
+ *                      (`iter<0`), `A` contains the factors L and U from A = P*L*U.
+ * @param[in]     lda   The leading dimension of `A`. `lda>=max(1,n)`.
+ * @param[out]    ipiv  Array of dimension `n`.
+ *                      The pivot indices.
+ * @param[in]     B     Array of dimension (`ldb`, `nrhs`).
+ *                      The `n`-by-`nrhs` right hand side matrix `B`.
+ * @param[in]     ldb   The leading dimension of `B`. `ldb>=max(1,n)`.
+ * @param[out]    X     Array of dimension (`ldx`, `nrhs`).
+ *                      If `info=0`, the `n`-by-`nrhs` solution matrix X.
+ * @param[in]     ldx   The leading dimension of `X`. `ldx>=max(1,n)`.
  * @param[out]    work  Double precision workspace for residual vectors.
- *                      Array of dimension (n, nrhs).
+ *                      Array of dimension (`n`, `nrhs`).
  * @param[out]    swork Single precision workspace for matrix and solutions.
- *                      Array of dimension n*(n+nrhs).
+ *                      Array of dimension `n*(n+nrhs)`.
  * @param[out]    iter  Iteration count:
- *                      - < 0: iterative refinement has failed, f64 precision
+ *                      - `iter<0`: iterative refinement has failed, double precision
  *                        factorization has been performed
  *                        - -1 : the routine fell back to full precision for
  *                          implementation- or machine-specific reasons
@@ -50,13 +56,12 @@
  *                        - -3 : failure of SGETRF
  *                        - -31: stop the iterative refinement after the 30th
  *                          iterations
- *                      - > 0: iterative refinement has been successfully used.
+ *                      - `iter>0`: iterative refinement has been successfully used.
  *                        Returns the number of iterations
  * @param[out]    info
- *                           Exit status:
- *                           - = 0: successful exit
- *                           - < 0: if info = -i, argument i had an illegal value
- *                           - > 0: if info = i, U(i-1,i-1) is exactly zero
+ *                          - `info=0`: successful exit
+ *                          - `info<0`: if `info=-i`, argument i had an illegal value
+ *                          - `info>0`: if `info=i`, U(i,i) is exactly zero
  */
 void dsgesv(
     const INT n,
