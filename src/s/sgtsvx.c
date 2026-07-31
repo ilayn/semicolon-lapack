79a63b26321df731ac2d955596585cb6104aedbb
ilayn <ilhanpolat@gmail.com>
2026-07-31 23:13:35 +0200

    DOC: Overhaul gtsvx family

--- src/s/sgtsvx.c
+++ src/s/sgtsvx.c
@@ -10,48 +10,89 @@
 
 /**
  * SGTSVX uses the LU factorization to compute the solution to a real
- * system of linear equations A * X = B or A**T * X = B,
- * where A is a tridiagonal matrix of order N and X and B are N-by-NRHS
+ * system of linear equations
+ * @rst
+ * .. code-block:: text
+ *
+ *     A * X = B  or  A**T * X = B
+ * @endrst
+ * where A is a tridiagonal matrix of order `n` and X and `B` are `n`-by-`nrhs`
  * matrices.
  *
  * Error bounds on the solution and a condition estimate are also provided.
  *
- * @param[in]     fact   Specifies whether the factored form of A has been supplied.
- *                       = 'F': DLF, DF, DUF, DU2, and IPIV contain the factored form.
- *                       = 'N': The matrix will be copied and factored.
+ * @rst
+ * The following steps are performed:
+ *
+ * 1. If ``fact='N'``, the LU decomposition is used to factor the matrix A
+ *    as A = L * U, where L is a product of permutation and unit lower
+ *    bidiagonal matrices and U is upper triangular with nonzeros in
+ *    only the main diagonal and first two superdiagonals.
+ *
+ * 2. If some ``U(i,i)=0``, so that U is exactly singular, then the routine
+ *    returns with ``info=i``. Otherwise, the factored form of A is used
+ *    to estimate the condition number of the matrix A. If the reciprocal
+ *    of the condition number is less than machine precision, ``info=n+1``
+ *    is returned as a warning, but the routine still goes on to solve for
+ *    X and compute error bounds as described below.
+ *
+ * 3. The system of equations is solved for X using the factored form of A.
+ *
+ * 4. Iterative refinement is applied to improve the computed solution
+ *    matrix and calculate error bounds and backward error estimates for it.
+ * @endrst
+ *
+ * @param[in]     fact
+ *                       - `'F'`: `DLF`, `DF`, `DUF`, `DU2`, and `ipiv` contain the
+ *                         factored form of A.
+ *                       - `'N'`: The matrix will be copied and factored.
  * @param[in]     trans  Specifies the form of the system of equations:
- *                       = 'N': A * X = B     (No transpose)
- *                       = 'T': A**T * X = B  (Transpose)
- *                       = 'C': A**H * X = B  (Conjugate transpose = Transpose)
- * @param[in]     n      The order of the matrix A. n >= 0.
- * @param[in]     nrhs   The number of right hand sides. nrhs >= 0.
- * @param[in]     DL     The (n-1) subdiagonal elements of A. Array of dimension (n-1).
- * @param[in]     D      The diagonal elements of A. Array of dimension (n).
- * @param[in]     DU     The (n-1) superdiagonal elements of A. Array of dimension (n-1).
- * @param[in,out] DLF    If fact = "F", the (n-1) multipliers from LU factorization.
- *                       If fact = "N", output. Array of dimension (n-1).
- * @param[in,out] DF     If fact = "F", the n diagonal elements of U.
- *                       If fact = "N", output. Array of dimension (n).
- * @param[in,out] DUF    If fact = "F", the (n-1) elements of first superdiagonal of U.
- *                       If fact = "N", output. Array of dimension (n-1).
- * @param[in,out] DU2    If fact = "F", the (n-2) elements of second superdiagonal of U.
- *                       If fact = "N", output. Array of dimension (n-2).
- * @param[in,out] ipiv   If fact = "F", the pivot indices from factorization.
- *                       If fact = "N", output. Array of dimension (n).
- * @param[in]     B      The N-by-NRHS right hand side matrix. Array of dimension (ldb, nrhs).
- * @param[in]     ldb    The leading dimension of B. ldb >= max(1, n).
- * @param[out]    X      The N-by-NRHS solution matrix. Array of dimension (ldx, nrhs).
- * @param[in]     ldx    The leading dimension of X. ldx >= max(1, n).
+ *                       - `'N'`: A * X = B (No transpose)
+ *                       - `'T'`: A**T * X = B (Transpose)
+ *                       - `'C'`: A**H * X = B (Conjugate transpose = Transpose)
+ * @param[in]     n      The order of the matrix A. `n>=0`.
+ * @param[in]     nrhs   The number of right hand sides. `nrhs>=0`.
+ * @param[in]     DL     Array of dimension (`n-1`).
+ *                       The (n-1) subdiagonal elements of A.
+ * @param[in]     D      Array of dimension (`n`).
+ *                       The diagonal elements of A.
+ * @param[in]     DU     Array of dimension (`n-1`).
+ *                       The (n-1) superdiagonal elements of A.
+ * @param[in,out] DLF    Array of dimension (`n-1`).
+ *                       If `fact='F'`, the (n-1) multipliers from LU factorization.
+ *                       If `fact='N'`, output.
+ * @param[in,out] DF     Array of dimension (`n`).
+ *                       If `fact='F'`, the n diagonal elements of U.
+ *                       If `fact='N'`, output.
+ * @param[in,out] DUF    Array of dimension (`n-1`).
+ *                       If `fact='F'`, the (n-1) elements of first superdiagonal of U.
+ *                       If `fact='N'`, output.
+ * @param[in,out] DU2    Array of dimension (`n-2`).
+ *                       If `fact='F'`, the (n-2) elements of second superdiagonal of U.
+ *                       If `fact='N'`, output.
+ * @param[in,out] ipiv   Array of dimension (`n`).
+ *                       If `fact='F'`, the pivot indices from factorization.
+ *                       If `fact='N'`, output.
+ * @param[in]     B      Array of dimension (`ldb`, `nrhs`).
+ *                       The `n`-by-`nrhs` right hand side matrix.
+ * @param[in]     ldb    The leading dimension of `B`. `ldb>=max(1,n)`.
+ * @param[out]    X      Array of dimension (`ldx`, `nrhs`).
+ *                       The `n`-by-`nrhs` solution matrix.
+ * @param[in]     ldx    The leading dimension of `X`. `ldx>=max(1,n)`.
  * @param[out]    rcond  The reciprocal condition number estimate.
- * @param[out]    ferr   Forward error bounds for each solution vector. Array of dimension (nrhs).
- * @param[out]    berr   Backward error for each solution vector. Array of dimension (nrhs).
- * @param[out]    work   Workspace array of dimension (3*n).
- * @param[out]    iwork  Integer workspace array of dimension (n).
+ * @param[out]    ferr   Array of dimension (`nrhs`).
+ *                       Forward error bounds for each solution vector.
+ * @param[out]    berr   Array of dimension (`nrhs`).
+ *                       Backward error for each solution vector.
+ * @param[out]    work   Workspace array of dimension (`3*n`).
+ * @param[out]    iwork  Integer workspace array of dimension (`n`).
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -i, the i-th argument had an illegal value
- *                         - > 0: if info = i (i <= n), U(i,i) is exactly zero
- *                         - = n+1: U is nonsingular, but rcond < machine precision
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-i`, the i-th argument had an illegal
+ *                           value
+ *                         - `info>0`: if `info=i` (i <= `n`), U(i,i) is exactly zero
+ *                         - `info=n+1`: U is nonsingular, but `rcond` < machine
+ *                           precision
  */
 void sgtsvx(
     const char* fact,
