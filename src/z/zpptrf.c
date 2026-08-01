c1872182400e7d4eef2904b2aaae4686768bb28a
ilayn <ilhanpolat@gmail.com>
2026-08-01 23:53:11 +0200

    DOC: Overhaul pptrf family

--- src/z/zpptrf.c
+++ src/z/zpptrf.c
@@ -13,25 +13,57 @@
  * positive definite matrix A stored in packed format.
  *
  * The factorization has the form
- *    A = U**H * U,  if UPLO = 'U', or
- *    A = L  * L**H,  if UPLO = 'L',
+ * @rst
+ * .. code-block:: text
+ *
+ *     A = U**H * U,  if uplo = 'U', or
+ *     A = L  * L**H,  if uplo = 'L',
+ * @endrst
  * where U is an upper triangular matrix and L is lower triangular.
  *
- * @param[in]     uplo   = 'U': Upper triangle of A is stored;
- *                        = 'L': Lower triangle of A is stored.
- * @param[in]     n      The order of the matrix A. n >= 0.
- * @param[in,out] AP     On entry, the upper or lower triangle of the Hermitian
- *                       matrix A, packed columnwise in a linear array.
- *                       On exit, if info = 0, the triangular factor U or L from
+ * @param[in]     uplo
+ *                       - `'U'`: Upper triangle of A is stored
+ *                       - `'L'`: Lower triangle of A is stored
+ * @param[in]     n      The order of the matrix A. `n>=0`.
+ * @param[in,out] AP     Array of dimension `n*(n+1)/2`.
+ *                       On entry, the upper or lower triangle of the Hermitian
+ *                       matrix A, packed columnwise in a linear array. The
+ *                       j-th column of A is stored in the array AP as follows:
+ *                       if `uplo='U'`, `AP[i + j*(j+1)/2] = A(i,j)` for `0<=i<=j`;
+ *                       if `uplo='L'`, `AP[i + j*(2*n-j-1)/2] = A(i,j)` for
+ *                       `j<=i<=n-1`.
+ *                       See below for further details.
+ *                       On exit, if `info=0`, the triangular factor U or L from
  *                       the Cholesky factorization A = U**H*U or A = L*L**H,
  *                       in the same storage format as A.
- *                       Array of dimension (n*(n+1)/2).
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -i, the i-th argument had an illegal value
- *                         - > 0: if info = i, the leading principal minor of order i
- *                           is not positive, and the factorization could not be
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-i`, the i-th argument had an illegal
+ *                           value
+ *                         - `info>0`: if `info=i`, the leading principal minor of order
+ *                           i is not positive, and the factorization could not be
  *                           completed.
+ *
+ * @par Further Details:
+ * @rst
+ * The packed storage scheme is illustrated by the following example
+ * when ``n=4``, ``uplo='U'``:
+ *
+ * Two-dimensional storage of the Hermitian matrix A:
+ *
+ * .. code-block:: text
+ *
+ *     a00 a01 a02 a03
+ *         a11 a12 a13
+ *             a22 a23     (aij = conjg(aji))
+ *                 a33
+ *
+ * Packed storage of the upper triangle of A:
+ *
+ * .. code-block:: text
+ *
+ *     AP = [ a00, a01, a11, a02, a12, a22, a03, a13, a23, a33 ]
+ * @endrst
  */
 void zpptrf(
     const char* uplo,
