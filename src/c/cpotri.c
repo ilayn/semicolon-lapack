d4fe1d32067e29a183e0863601a939b890052d04
ilayn <ilhanpolat@gmail.com>
2026-08-01 11:39:28 +0200

    DOC: Overhaul potri family

--- src/c/cpotri.c
+++ src/c/cpotri.c
@@ -11,22 +11,22 @@
  * matrix A using the Cholesky factorization A = U**H*U or A = L*L**H
  * computed by CPOTRF.
  *
- * @param[in]     uplo  Specifies whether the factor stored in A is upper or
- *                      lower triangular.
- *                      = 'U': Upper triangle of A is stored
- *                      = 'L': Lower triangle of A is stored
- * @param[in]     n     The order of the matrix A. n >= 0.
- * @param[in,out] A     On entry, the triangular factor U or L from the
+ * @param[in]     uplo
+ *                      - `'U'`: Upper triangle of A is stored
+ *                      - `'L'`: Lower triangle of A is stored
+ * @param[in]     n     The order of the matrix A. `n>=0`.
+ * @param[in,out] A     Array of dimension (`lda`, `n`).
+ *                      On entry, the triangular factor U or L from the
  *                      Cholesky factorization A = U**H*U or A = L*L**H,
- *                      as computed by cpotrf.
+ *                      as computed by `cpotrf`.
  *                      On exit, the upper or lower triangle of the (Hermitian)
  *                      inverse of A, overwriting the input factor U or L.
- *                      Array of dimension (lda, n).
- * @param[in]     lda   The leading dimension of the array A. lda >= max(1, n).
+ * @param[in]     lda   The leading dimension of the array `A`. `lda>=max(1,n)`.
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -k, the k-th argument had an illegal value
- *                         - > 0: if info = k, the (k,k) element of the factor U or L
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-k`, the k-th argument had an illegal
+ *                           value
+ *                         - `info>0`: if `info=k`, the (k,k) element of the factor U or L
  *                           is zero, and the inverse could not be computed.
  */
 void cpotri(
