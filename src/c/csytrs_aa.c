21d0a0f789f00033db4decdfb0cb1e294e0ad6b1
ilayn <ilhanpolat@gmail.com>
2026-08-01 23:53:12 +0200

    DOC: Overhaul sytrs_aa family

--- src/c/csytrs_aa.c
+++ src/c/csytrs_aa.c
@@ -12,49 +12,34 @@
  * symmetric matrix A using the factorization A = U**T*T*U or
  * A = L*T*L**T computed by CSYTRF_AA.
  *
- * @param[in] uplo
- *          Specifies whether the details of the factorization are stored
- *          as an upper or lower triangular matrix.
- *          = 'U':  Upper triangular, form is A = U**T*T*U;
- *          = 'L':  Lower triangular, form is A = L*T*L**T.
- *
- * @param[in] n
- *          The order of the matrix A. n >= 0.
- *
- * @param[in] nrhs
- *          The number of right hand sides, i.e., the number of columns
- *          of the matrix B. nrhs >= 0.
- *
- * @param[in] A
- *          Single complex array, dimension (lda, n).
- *          Details of factors computed by CSYTRF_AA.
- *
- * @param[in] lda
- *          The leading dimension of the array A. lda >= max(1, n).
- *
- * @param[in] ipiv
- *          Integer array, dimension (n).
- *          Details of the interchanges as computed by CSYTRF_AA.
- *
- * @param[in,out] B
- *          Single complex array, dimension (ldb, nrhs).
- *          On entry, the right hand side matrix B.
- *          On exit, the solution matrix X.
- *
- * @param[in] ldb
- *          The leading dimension of the array B. ldb >= max(1, n).
- *
- * @param[out] work
- *          Single complex array, dimension (max(1, lwork)).
- *
- * @param[in] lwork
- *          The dimension of the array work.
- *          If min(n, nrhs) = 0, lwork >= 1, else lwork >= 3*n-2.
- *          If lwork = -1, then a workspace query is assumed.
- *
- * @param[out] info
- *                         - = 0:  successful exit
- *                         - < 0:  if info = -i, the i-th argument had an illegal value
+ * @param[in]     uplo
+ *                      - `'U'`: Upper triangular, form is A = U**T*T*U
+ *                      - `'L'`: Lower triangular, form is A = L*T*L**T
+ * @param[in]     n     The order of the matrix A. `n>=0`.
+ * @param[in]     nrhs  The number of right hand sides, i.e., the number of
+ *                      columns of the matrix B. `nrhs>=0`.
+ * @param[in]     A     Complex array of dimension `(lda,n)`.
+ *                      Details of factors computed by `csytrf_aa`.
+ * @param[in]     lda   The leading dimension of the array A. `lda>=max(1,n)`.
+ * @param[in]     ipiv  Array of dimension `n`.
+ *                      Details of the interchanges as computed by
+ *                      `csytrf_aa`.
+ * @param[in,out] B     Complex array of dimension `(ldb,nrhs)`.
+ *                      On entry, the right hand side matrix B.
+ *                      On exit, the solution matrix X.
+ * @param[in]     ldb   The leading dimension of the array B. `ldb>=max(1,n)`.
+ * @param[out]    work  Complex array of dimension `max(1,lwork)`.
+ * @param[in]     lwork The dimension of the array `work`.
+ *                      If `min(n,nrhs)=0`, `lwork>=1`, else `lwork>=3*n-2`.
+ *                      If `lwork=-1`, then a workspace query is assumed; the
+ *                      routine only calculates the minimal size of the
+ *                      `work` array, returns this value as the first entry
+ *                      of the `work` array, and no error message related to
+ *                      `lwork` is issued.
+ * @param[out]    info
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-i`, the i-th argument had an illegal
+ *                           value
  */
 void csytrs_aa(
     const char* uplo,
