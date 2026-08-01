e1b7f9067bd9ff35de79145b3c90eea7ef3202cf
ilayn <ilhanpolat@gmail.com>
2026-08-01 23:53:11 +0200

    DOC: Overhaul pttrs family

--- src/s/spttrs.c
+++ src/s/spttrs.c
@@ -14,26 +14,27 @@
  * matrix whose subdiagonal is specified in the vector E, and X and B
  * are N by NRHS matrices.
  *
- * @param[in]     n     The order of the tridiagonal matrix A. n >= 0.
+ * @param[in]     n     The order of the tridiagonal matrix A. `n>=0`.
  * @param[in]     nrhs  The number of right hand sides, i.e., the number
- *                      of columns of the matrix B. nrhs >= 0.
- * @param[in]     D     Double precision array, dimension (n).
+ *                      of columns of the matrix B. `nrhs>=0`.
+ * @param[in]     D     Array of dimension `n`.
  *                      The n diagonal elements of the diagonal matrix D
  *                      from the L*D*L**T factorization of A.
- * @param[in]     E     Double precision array, dimension (n-1).
+ * @param[in]     E     Array of dimension `n-1`.
  *                      The (n-1) subdiagonal elements of the unit bidiagonal
  *                      factor L from the L*D*L**T factorization of A.
  *                      E can also be regarded as the superdiagonal of the
  *                      unit bidiagonal factor U from the factorization
  *                      A = U**T*D*U.
- * @param[in,out] B     Double precision array, dimension (ldb, nrhs).
+ * @param[in,out] B     Array of dimension `(ldb,nrhs)`.
  *                      On entry, the right hand side vectors B for the
  *                      system of linear equations.
  *                      On exit, the solution vectors, X.
- * @param[in]     ldb   The leading dimension of the array B. ldb >= max(1,n).
+ * @param[in]     ldb   The leading dimension of the array B. `ldb>=max(1,n)`.
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -k, the k-th argument had an illegal value
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-k`, the k-th argument had an illegal
+ *                           value
  */
 void spttrs(
     const INT n,
