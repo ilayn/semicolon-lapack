fe0c173a9c302698b68b09e2fe0605edc912122b
ilayn <ilhanpolat@gmail.com>
2026-08-01 23:53:11 +0200

    DOC: Overhaul pptrs family

--- src/d/dpptrs.c
+++ src/d/dpptrs.c
@@ -11,22 +11,28 @@
  * positive definite matrix A in packed storage using the Cholesky
  * factorization A = U**T*U or A = L*L**T computed by DPPTRF.
  *
- * @param[in]     uplo   = 'U': Upper triangle of A is stored;
- *                        = 'L': Lower triangle of A is stored.
- * @param[in]     n      The order of the matrix A. n >= 0.
+ * @param[in]     uplo
+ *                       - `'U'`: Upper triangle of A is stored
+ *                       - `'L'`: Lower triangle of A is stored
+ * @param[in]     n      The order of the matrix A. `n>=0`.
  * @param[in]     nrhs   The number of right hand sides, i.e., the number of
- *                       columns of the matrix B. nrhs >= 0.
- * @param[in]     AP     The triangular factor U or L from the Cholesky
+ *                       columns of the matrix `B`. `nrhs>=0`.
+ * @param[in]     AP     Array of dimension `n*(n+1)/2`.
+ *                       The triangular factor U or L from the Cholesky
  *                       factorization A = U**T*U or A = L*L**T, packed
- *                       columnwise in a linear array.
- *                       Array of dimension (n*(n+1)/2).
- * @param[in,out] B      On entry, the right hand side matrix B.
+ *                       columnwise in a linear array. The j-th column of
+ *                       U or L is stored in the array AP as follows:
+ *                       if `uplo='U'`, `AP[i + j*(j+1)/2] = U(i,j)` for `0<=i<=j`;
+ *                       if `uplo='L'`, `AP[i + j*(2*n-j-1)/2] = L(i,j)` for
+ *                       `j<=i<=n-1`.
+ * @param[in,out] B      Array of dimension (`ldb`, `nrhs`).
+ *                       On entry, the right hand side matrix `B`.
  *                       On exit, the solution matrix X.
- *                       Array of dimension (ldb, nrhs).
- * @param[in]     ldb    The leading dimension of the array B. ldb >= max(1,n).
+ * @param[in]     ldb    The leading dimension of the array `B`. `ldb>=max(1,n)`.
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -i, the i-th argument had an illegal value
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-i`, the i-th argument had an illegal
+ *                           value
  */
 void dpptrs(
     const char* uplo,
