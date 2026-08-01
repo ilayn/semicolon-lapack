a99e7e5831b7edd094f363388c1725412853f9eb
ilayn <ilhanpolat@gmail.com>
2026-08-01 11:39:28 +0200

    DOC: Overhaul posv. family

--- src/d/dposv.c
+++ src/d/dposv.c
@@ -8,35 +8,51 @@
 
 /**
  * DPOSV computes the solution to a real system of linear equations
- *    A * X = B,
- * where A is an N-by-N symmetric positive definite matrix and X and B
- * are N-by-NRHS matrices.
+ * @rst
+ * .. code-block:: text
+ *
+ *     A * X = B
+ * @endrst
+ * where A is an `n`-by-`n` symmetric positive definite matrix and X and `B`
+ * are `n`-by-`nrhs` matrices.
  *
  * The Cholesky decomposition is used to factor A as
- *    A = U**T * U, if UPLO = 'U', or
- *    A = L * L**T, if UPLO = 'L',
+ * @rst
+ * .. code-block:: text
+ *
+ *     A = U**T * U, if uplo = 'U', or
+ *     A = L * L**T, if uplo = 'L',
+ * @endrst
  * where U is an upper triangular matrix and L is a lower triangular
  * matrix. The factored form of A is then used to solve the system of
  * equations A * X = B.
  *
- * @param[in]     uplo  = 'U': Upper triangle of A is stored
- *                       = 'L': Lower triangle of A is stored
- * @param[in]     n     The number of linear equations. n >= 0.
- * @param[in]     nrhs  The number of right hand sides. nrhs >= 0.
- * @param[in,out] A     On entry, the symmetric matrix A.
- *                      On exit, if info = 0, the factor U or L from the
- *                      Cholesky factorization A = U**T*U or A = L*L**T.
- *                      Array of dimension (lda, n).
- * @param[in]     lda   The leading dimension of the array A. lda >= max(1, n).
- * @param[in,out] B     On entry, the N-by-NRHS right hand side matrix B.
- *                      On exit, if info = 0, the N-by-NRHS solution matrix X.
- *                      Array of dimension (ldb, nrhs).
- * @param[in]     ldb   The leading dimension of the array B. ldb >= max(1, n).
+ * @param[in]     uplo
+ *                      - `'U'`: Upper triangle of A is stored
+ *                      - `'L'`: Lower triangle of A is stored
+ * @param[in]     n     The number of linear equations. `n>=0`.
+ * @param[in]     nrhs  The number of right hand sides. `nrhs>=0`.
+ * @param[in,out] A     Array of dimension (`lda`, `n`).
+ *                      On entry, the symmetric matrix A. If `uplo='U'`, the
+ *                      leading `n`-by-`n` upper triangular part of A contains the
+ *                      upper triangular part of the matrix A, and the strictly
+ *                      lower triangular part of A is not referenced. If `uplo='L'`,
+ *                      the leading `n`-by-`n` lower triangular part of A contains
+ *                      the lower triangular part of the matrix A, and the strictly
+ *                      upper triangular part of A is not referenced.
+ *                      On exit, if `info=0`, the factor U or L from the Cholesky
+ *                      factorization A = U**T*U or A = L*L**T.
+ * @param[in]     lda   The leading dimension of the array `A`. `lda>=max(1,n)`.
+ * @param[in,out] B     Array of dimension (`ldb`, `nrhs`).
+ *                      On entry, the `n`-by-`nrhs` right hand side matrix `B`.
+ *                      On exit, if `info=0`, the `n`-by-`nrhs` solution matrix X.
+ * @param[in]     ldb   The leading dimension of the array `B`. `ldb>=max(1,n)`.
  * @param[out]    info
- *                         - = 0: successful exit
- *                         - < 0: if info = -k, the k-th argument had an illegal value
- *                         - > 0: if info = k, the leading principal minor of order k
- *                           of A is not positive, so the factorization could not
+ *                         - `info=0`: successful exit
+ *                         - `info<0`: if `info=-k`, the k-th argument had an illegal
+ *                           value
+ *                         - `info>0`: if `info=k`, the leading principal minor of order
+ *                           k of A is not positive, so the factorization could not
  *                           be completed, and the solution has not been computed.
  */
 void dposv(
