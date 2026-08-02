f430628f05add00526218fc73762a0aefcabf954
ilayn <ilhanpolat@gmail.com>
2026-08-02 12:39:13 +0200

    DOC: Add missing codeblocks to  lasyf_rk family

--- src/d/dlasyf_rk.c
+++ src/d/dlasyf_rk.c
@@ -11,12 +11,15 @@
  * DLASYF_RK computes a partial factorization of a real symmetric
  * matrix A using the bounded Bunch-Kaufman (rook) diagonal
  * pivoting method. The partial factorization has the form:
+ * @rst
+ * .. code-block:: text
  *
- * A  =  ( I  U12 ) ( A11  0  ) (  I       0    )  if UPLO = 'U', or:
- *       ( 0  U22 ) (  0   D  ) ( U12**T U22**T )
+ *     A  =  ( I  U12 ) ( A11  0  ) (  I       0    )  if UPLO = 'U', or:
+ *           ( 0  U22 ) (  0   D  ) ( U12**T U22**T )
  *
- * A  =  ( L11  0 ) (  D   0  ) ( L11**T L21**T )  if UPLO = 'L',
- *       ( L21  I ) (  0  A22 ) (  0       I    )
+ *     A  =  ( L11  0 ) (  D   0  ) ( L11**T L21**T )  if UPLO = 'L',
+ *           ( L21  I ) (  0  A22 ) (  0       I    )
+ * @endrst
  *
  * where the order of D is at most NB. The actual order is returned in
  * the argument KB, and is either NB or NB-1, or N if N <= NB.
