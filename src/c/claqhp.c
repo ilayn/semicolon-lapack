ecc26f879ed16b17a9ed2f18510b3ee810951790
ilayn <ilhanpolat@gmail.com>
2026-08-02 12:26:48 +0200

    DOC: Overhaul  laqhp family

--- src/c/claqhp.c
+++ src/c/claqhp.c
@@ -10,24 +10,32 @@
  * CLAQHP equilibrates a Hermitian matrix A using the scaling factors
  * in the vector S.
  *
- * @param[in]     uplo   = 'U': Upper triangular part of A is stored
- *                        = 'L': Lower triangular part of A is stored
- * @param[in]     n      The order of the matrix A. n >= 0.
+ * @param[in]     uplo
+ *                       - `'U'`: Upper triangular part of A is stored
+ *                       - `'L'`: Lower triangular part of A is stored
+ * @param[in]     n      The order of the matrix A. `n>=0`.
  * @param[in,out] AP     On entry, the upper or lower triangle of the Hermitian
  *                       matrix A, packed columnwise in a linear array.
  *                       The j-th column of A is stored in the array AP as follows:
- *                       if uplo = 'U', AP(i + (j-1)*j/2) = A(i,j) for 0<=i<=j;
- *                       if uplo = 'L', AP(i + (j-1)*(2n-j)/2) = A(i,j) for j<=i<n.
+ *                       if `uplo='U'`, `AP[i + j*(j+1)/2] = A(i,j)` for
+ *                       `0<=i<=j`; if `uplo='L'`,
+ *                       `AP[i - j + j*(2*n-j+1)/2] = A(i,j)` for `j<=i<n`.
  *                       On exit, the equilibrated matrix: diag(S) * A * diag(S),
  *                       in the same storage format as A.
- *                       Array of dimension (n*(n+1)/2).
- * @param[in]     S      The scale factors for A. Array of dimension (n).
+ *                       Array of dimension `n*(n+1)/2`.
+ * @param[in]     S      The scale factors for A. Array of dimension `n`.
  * @param[in]     scond  Ratio of the smallest S(i) to the largest S(i).
  * @param[in]     amax   Absolute value of largest matrix entry.
- * @param[out]    equed  Specifies whether or not equilibration was done:
- *                       = 'N': No equilibration
- *                       = 'Y': Equilibration was done, i.e., A has been replaced
+ * @param[out]    equed
+ *                       - `'N'`: No equilibration
+ *                       - `'Y'`: Equilibration was done, i.e., A has been replaced
  *                              by diag(S) * A * diag(S).
+ *
+ * @par Internal Parameters:
+ * `THRESH` decides whether to scale based on the ratio of the scaling
+ * factors: scaling is performed when `scond<THRESH`. `LARGE` and `SMALL`
+ * decide whether to scale based on the absolute size of the largest
+ * matrix element: scaling is performed when `amax>LARGE` or `amax<SMALL`.
  */
 void claqhp(
     const char* uplo,
